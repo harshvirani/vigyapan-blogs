06366c53253b0674fcac291328ca1a2f383af415
harsh-virani <Iloveyou@3313>
2022-09-15 23:31:00 +0530

    minor changes related to vpn added

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,12 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    function printJson($message, $success = false,  $data = null)
+    {
+        return response()->json([
+            "success" => $success,
+            "message" => $message,
+            "data" => $data,
+        ]);
+    }
 }
