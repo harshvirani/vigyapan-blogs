4f67c5cc5b688eca529c7ead66a0055828b9eb53
Harsh Virani <harshvirani33@gmail.com>
2022-12-28 17:06:52 +0530

    removed unnecessory logs

--- app/Http/Middleware/RequestTrack.php
+++ app/Http/Middleware/RequestTrack.php
@@ -16,10 +16,6 @@ class RequestTrack
      */
     public function handle(Request $request, Closure $next)
     {
-        $ipAddress = $request->ip();
-        \Log::info("IP ADDRESS====".$ipAddress);
-        \Log::info("PATH====".$request->getPathInfo());
-        \Log::info("PATH====".json_encode($request->all()));
         \App\Models\RequestTrack::query()->create([
             'route'=>$request->getPathInfo(),
             'request_data'=>$request->all(),
